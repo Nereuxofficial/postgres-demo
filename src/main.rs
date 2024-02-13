15f7280649075a4418f9703f0197022fa5be1299
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-02-13 17:05:31 +0100

    hi postgres

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use sqlx::postgres::PgPoolOptions;
+// use sqlx::mysql::MySqlPoolOptions;
+// etc.
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    // Create a connection pool
+    //  for MySQL/MariaDB, use MySqlPoolOptions::new()
+    //  for SQLite, use SqlitePoolOptions::new()
+    //  etc.
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@localhost/db")
+        .await?;
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
+}
